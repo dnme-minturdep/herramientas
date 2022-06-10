e90f45defb859d99542a8e5735d34650271edc55
jgjuara <jg.juara@gmail.com>
2022-06-10 13:24:31 -0300

    correccion de nombres de tests

--- tests/testthat/test-limpiar_texto.R
+++ tests/testthat/test-limpiar_texto.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("limpiar texto", {
   expect_equal(limpiar_texto("TéxTÔ con una Ñ?!"), "texto con una n")
   expect_equal(limpiar_texto("TéxTÔ con una Ñ?!", enie = F), "texto con una ñ")
 })
--- tests/testthat/test-numero_en_palabras.R
+++ tests/testthat/test-numero_en_palabras.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("numeros en palabras", {
   expect_equal(numeros_en_palabras("25 de mayo"), "veinticinco de mayo")
   expect_equal(numeros_en_palabras("6 de abril del 98"), "seis de abril del noventa y ocho")
 })
