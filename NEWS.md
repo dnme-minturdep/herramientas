cc8fc0c37c471eed57d87290a048583db32cba69
jgjuara <jg.juara@gmail.com>
2023-01-10 16:35:39 -0300

    mejora remover_tildes

--- NEWS.md
+++ NEWS.md
@@ -1,10 +1,14 @@
+# herramientas 0.0.0.9003
+
+* Mejora `remover_tildes()` limpia tildes de mayúsculas también.
+
 # herramientas 0.0.0.9002
 
-* Parametro "uncompressed" por default para write_file_srv desde local.
+* Parametro "uncompressed" por default para `write_file_srv()` desde local.
 
 # herramientas 0.0.0.9001
 
-* Incorporo las funciones de lectura (read_file_srv) y escritura (write_file_srv) desde el server
+* Incorporo las funciones de lectura `read_file_srv()` y escritura `write_file_srv()` desde el server
 
 # herramientas 0.0.0.9
 
--- R/remover_tildes.R
+++ R/remover_tildes.R
@@ -1,14 +1,14 @@
 
 #' Limpieza general de tildes:
 #' @description
-#' Elimina tildes y dieresis de las vocales y la letra "y". Ej.: "ÿúòâ" en "yuoa".
+#' Elimina tildes y dieresis de las vocales y la letra "y". Ej.: "ÿÚòâ" en "yUoa".
 #'
 #' @param texto variable de clase 'character' a la que se le quiere aplicar la función
 #' @examples
-#' remover_tildes("ÿúòâ") #"yuoa"
+#' remover_tildes("ÿÚòâ") #"yuoa"
 #'
 #'@export
 remover_tildes <- function(texto) {
-  mgsub::mgsub(texto, pattern = acentos_df$ansi,
-               replacement = acentos_df$limpio, fixed = T)
+  mgsub::mgsub(texto, pattern = c(acentos_df$ansi, toupper(acentos_df$ansi)),
+               replacement = c(acentos_df$limpio, toupper(acentos_df$limpio)), fixed = T)
 }
